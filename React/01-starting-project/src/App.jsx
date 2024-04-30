6852284223031e85f64c84df90fb0c3e43a0e9c9
yosriAguero <yosri.jebri@gmail.com>
2024-04-30 15:27:12 +0200

    Header in separate component

--- React/01-starting-project/src/App.jsx
+++ React/01-starting-project/src/App.jsx
@@ -1,18 +1,4 @@
-function Header()
-{
-  return (
-    <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      Fundamental React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>
-  );
-}
-
-
+import Header from "./Header";
 
 function App() {
   return (
